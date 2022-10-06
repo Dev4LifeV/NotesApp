d94d61aab38107531b733bc30d6c3f9931dd8efc
Dev4LifeV <vctudo.27@gmail.com>
2022-10-06 00:22:59 -0300

    🎨 Improved DI

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/app.dart';
+import 'package:notes_app/injector.dart';
 
-void main() => runApp(const App());
+void main() => runApp(Injector(child: const App()).init());
