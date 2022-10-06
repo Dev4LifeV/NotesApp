af72663883c81d85e2ba8775186fc8b1a26f016b
Dev4LifeV <vctudo.27@gmail.com>
2022-10-06 00:22:46 -0300

    🎨 Improved DI

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:notes_app/controller/controller_notes.dart';
 import 'package:notes_app/view/view_home.dart';
 
 class App extends MaterialApp {
   const App({Key? key}) : super(key: key);
 
   @override
-  Widget get home => ViewHome(ControllerNotes());
+  Widget get home => ViewHome();
 }
