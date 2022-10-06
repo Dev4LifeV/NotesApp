adca461ddffad3fa0ae207e7516ffda64a04ca73
Dev4LifeV <vctudo.27@gmail.com>
2022-10-06 00:22:54 -0300

    🎨 Improved DI

--- lib/injector.dart
+++ lib/injector.dart
@@ -1,12 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/controller/controller_notes.dart';
+import 'package:notes_app/repository/repository_notes.dart';
 
 class Injector extends InheritedWidget {
-  const Injector({required Widget child, Key? key})
-      : super(key: key, child: child);
+  Injector init() {
+    _repositoryNotes = RepositoryNotes();
+    _controllerNotes = ControllerNotes(_repositoryNotes);
+    return this;
+  }
+
+  late final ControllerNotes _controllerNotes;
+  late final RepositoryNotes _repositoryNotes;
+
+  Injector({
+    required Widget child,
+    Key? key,
+  }) : super(
+          key: key,
+          child: child,
+        );
 
   static Injector of(BuildContext context) =>
       context.dependOnInheritedWidgetOfExactType<Injector>() as Injector;
 
   @override
   bool updateShouldNotify(Injector old) => false;
+
+  ControllerNotes get controllerNotes => _controllerNotes;
+  RepositoryNotes get repositoryNotes => _repositoryNotes;
 }
