c14728db35a084874bd913f69dde1ed2761d49bf
Dev4LifeV <vctudo.27@gmail.com>
2022-10-06 00:22:49 -0300

    🎨 Improved DI

--- lib/controller/controller_notes.dart
+++ lib/controller/controller_notes.dart
@@ -2,7 +2,9 @@ import 'package:notes_app/model/note.dart';
 import 'package:notes_app/repository/repository_notes.dart';
 
 class ControllerNotes {
-  final RepositoryNotes _repo = RepositoryNotes();
+  ControllerNotes(this._repo);
+
+  final RepositoryNotes _repo;
 
   List<Note> listNotes() => _repo.listNotes();
 
