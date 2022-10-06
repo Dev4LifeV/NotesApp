465f9df27b7c4c9af71b6cf8405e0bea80c97258
Dev4LifeV <vctudo.27@gmail.com>
2022-10-05 23:27:43 -0300

    🎨 Removed App to another file

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:notes_app/view/view_home.dart';
+import 'package:notes_app/app.dart';
 
 void main() => runApp(const App());
-
-class App extends MaterialApp {
-  const App({Key? key}) : super(key: key);
-
-  @override
-  Widget get home => const ViewHome();
-}
